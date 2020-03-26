9e2972f3030adf40e3bb6400440c24b976ba3d0d
soroban-sensei <58226455+soroban-sensei@users.noreply.github.com>
2020-03-26 16:26:17 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Set constraints for the video stream
-var constraints = { video: { facingMode: "enviroment" }, audio: false };
+var constraints = { video: { facingMode: "environment" }, audio: false };
 var track = null;
 
 // Define constants
